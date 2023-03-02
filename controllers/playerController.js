c3466a36d61bb7bd66f34a4604961aacfb827e3b
Vennis <hoanganhgo28062001@gmail.com>
2023-03-02 22:57:13 +0700

    Last: ass2

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -1,75 +1,80 @@
 const Players = require("../models/player");
 
 const clubs = [
-    { id: "1", name: "Barcelona" },
-    { id: "2", name: "Real Madrid" },
-    { id: "3", name: "PSG" },
-    { id: "4", name: "Man United" },
-    { id: "5", name: "Asernal" },
-    { id: "6", name: "Man City" },
-    { id: "7", name: "Everton" },
-]
+  { id: "1", name: "Barcelona" },
+  { id: "2", name: "Real Madrid" },
+  { id: "3", name: "PSG" },
+  { id: "4", name: "Man United" },
+  { id: "5", name: "Asernal" },
+  { id: "6", name: "Man City" },
+  { id: "7", name: "Everton" },
+];
 
 const positions = [
-    { id: "1", name: "GK" },
-    { id: "2", name: "CB" },
-    { id: "3", name: "DF" },
-    { id: "4", name: "LM" },
-    { id: "5", name: "CD" },
-    { id: "6", name: "CM" },
-    { id: "7", name: "RM" },
-    { id: "8", name: "RF" },
-    { id: "9", name: "LF" },
-    { id: "10", name: "CF" },
-]
+  { id: "1", name: "Goalkeeper" },
+  { id: "2", name: "Defender" },
+  { id: "3", name: "Centreback" },
+  { id: "4", name: "Sweeper" },
+  { id: "5", name: "Fullback" },
+  { id: "6", name: "Wingback" },
+  { id: "7", name: "Midfielder" },
+  { id: "8", name: "Central midfielder" },
+  { id: "9", name: "Defensive midfielder" },
+  { id: "10", name: "Attacking midfielder" },
+  { id: "10", name: "Wide midfielder" },
+  { id: "10", name: "Striker" },
+];
 
 class playerController {
-    index(req, res, next) {
-        Players.find({})
-            .then((players) => {
-                res.render("players", {
-                    title: "The list of Players",
-                    players: players,
-                    positions: positions,
-                    clubs: clubs
-                });
-            })
-            .catch(next);
-    }
-    getPlayer(req, res, next) {
-        Players.findById(req.params.id)
-            .then((player) => {
-                res.render("getPlayer", {
-                    title: "The Player",
-                    player: player,
-                    positions: positions,
-                    clubs: clubs
-                });
-            })
-            .catch(next);
-    }
-    create(req, res, next) {
-        const player = new Players(req.body);
-        player
-            .save()
-            .then(() => res.redirect("/"))
-            .catch((error) => {});
-    }
-    update(req, res, next) {
-        Players.findByIdAndUpdate(req.params.id, req.body, function(err, result) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.redirect("/");
-            }
+  index(req, res, next) {
+    Players.find({})
+      .then((players) => {
+        res.render("players", {
+          title: "The list of Players",
+          players: players,
+          positions: positions,
+          clubs: clubs,
         });
-    }
-    delete(req, res, next) {
-        Players.findByIdAndRemove(req.params.id, function(err, player) {
-            if (err) throw err;
-            console.log("Success");
+      })
+      .catch(next);
+  }
+  getPlayer(req, res, next) {
+    Players.findById(req.params.id)
+      .then((player) => {
+        res.render("getPlayer", {
+          title: "The Player",
+          player: player,
+          positions: positions,
+          clubs: clubs,
         });
+      })
+      .catch(next);
+  }
+  create(req, res, next) {
+    const player = new Players(req.body);
+    player
+      .save()
+      .then(() => res.redirect("/"))
+      .catch((error) => {
+        res.render("error-custom");
+      });
+  }
+  update(req, res, next) {
+    Players.findByIdAndUpdate(req.params.id, req.body, function (err, result) {
+      if (err) {
+        console.log(err);
+        res.render("error-custom");
+      } else {
         res.redirect("/");
-    }
+      }
+    });
+  }
+  delete(req, res, next) {
+    Players.findByIdAndRemove(req.params.id, function (err, player) {
+      if (err) throw err;
+      console.log("Success");
+    });
+    res.redirect("/");
+  }
 }
 module.exports = new playerController();
--- models/nation.js
+++ models/nation.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-const nationSchema = mongoose.Schema({
+const nationSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     description: {
-        type: String,
-        required: true
-    }
-}, { timestamps: true });
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Nation", nationSchema);
